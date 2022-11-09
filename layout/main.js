0c5636feb0ffda45f1cdc9c2e07ae8ad1f7f987a
LeonardoPirchio <116616781+LeonardoPirchio@users.noreply.github.com>
2022-11-09 16:17:42 -0300

    Update

--- layout/main.js
+++ layout/main.js
@@ -1,29 +1,29 @@
 /* ========================
 * ====== Bar ========
 =========================== */
-$("html, body, main").css({
+$("html, body, .main").css({
     height: $(window).height()
 });
 
 /* ========================
 * ====== Menu ========
 =========================== */
-$('.main').scroll($.debounce( 1000, true, function(){
-    $('#menu').addClass('show');
+$(window).scroll($.debounce( 1000, true, function(){
+    $('.menu').addClass('show');
 } ) );
-$('.main').scroll($.debounce( 1000, function(){
-    $('#menu').removeClass('show');
+$(window).scroll($.debounce( 1000, function(){
+    $('.menu').removeClass('show');
 } ) );
 
-$('.main').scroll(function(event) {
+$(window).scroll(function(event) {
 	var scrollPos = $(document).scrollTop();
 	console.log(scrollPos);
-	$('#menu a').each(function() {
+	$('.menu a').each(function() {
 		var curLink = $(this);
 		var refElement = $(curLink.attr('href'));
 
 		if (refElement.position().top <= scrollPos + 60) {
-			$('#menu a').removeClass('active');
+			$('.menu a').removeClass('active');
 			curLink.addClass('active');
 
 		} else {
