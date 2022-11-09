bc8af55ba3d6ec35c899b02844ec8f9a46f1cf4c
LeonardoPirchio <116616781+LeonardoPirchio@users.noreply.github.com>
2022-11-09 01:31:47 -0300

    Main

---
+++ layout/main.js
@@ -0,0 +1,52 @@
+/* ========================
+* ====== Navegador ========
+=========================== */
+// const navSlide = () => {
+//     const burger = document.querySelector('#burger');
+//     const navegador = document.querySelector('#navegador');
+//     const navLinks = document.querySelectorAll('#navegador li');
+
+//     burger.addEventListener('click', () => {
+//         burger.classList.toggle('burLigado');
+//         navegador.classList.toggle('navLigado');
+
+//         navLinks.forEach((link, index) => {
+//             if (link.style.animation) {
+//                 link.style.animation = '';
+//             } else {
+//                 link.style.animation = `navFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+//             }
+//         });
+
+//         $('#navegador a').click(function() {
+//             $(burger).removeClass('burLigado');
+//             $(navegador).removeClass('navLigado');
+
+//             navLinks.forEach((link, index) => {
+//                 link.style.animation = '';
+//             });
+//         });
+//     });
+// }
+
+// navSlide();
+
+/* ========================
+* ====== Menu ========
+=========================== */
+$(window).scroll(function(event) {
+	var scrollPos = $(document).scrollTop();
+	console.log(scrollPos);
+	$('#menu a').each(function() {
+		var curLink = $(this);
+		var refElement = $(curLink.attr('href'));
+
+		if (refElement.position().top <= scrollPos + 60) {
+			$('#menu a').removeClass('active');
+			curLink.addClass('active');
+
+		} else {
+			curLink.removeClass('active');
+		}
+	});
+});
